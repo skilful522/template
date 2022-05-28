8fbbfce7571142b65674e22dffa548bc47a32f32
Stanislav Sergienya <skilful522@gmail.com>
2022-05-28 12:20:07 +0300

    initial commit

---
+++ webpack.config.js
@@ -0,0 +1,31 @@
+const path = require('path')
+const HTMLWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+
+module.exports = {
+    context: path.resolve(__dirname, 'src'),
+    mode: 'development',
+    entry: './index.js',
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'bundle.js'
+    },
+    plugins: [
+        new HTMLWebpackPlugin({
+            template: './index.html'
+        }),
+        new CleanWebpackPlugin()
+    ],
+    module: {
+        rules: [
+            {
+                test: /\.css$/,
+                use: ['style-loader', 'css-loader']
+            },
+            {
+                test: /\.(png|mp3|wav|ogg)$/,
+                use: ['file-loader']
+            }
+        ],
+    }
+}
